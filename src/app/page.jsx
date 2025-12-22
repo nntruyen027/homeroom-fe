c14b5ed7618e1c3181c4c9978e7be1dcdab3dbca
Nguyễn Ngọc Truyện <nntruyen027@gmail.com>
2025-12-22 08:41:06 +0700

    thêm dashboard giáo viên

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,14 @@
 'use client';
 
 import {useRouter} from 'next/navigation';
+import {useEffect} from "react";
 
 export default function Home() {
     const router = useRouter();
+
+    useEffect(() => {
+        router.replace("/login");
+    }, [[]])
 
     return (
         <div
