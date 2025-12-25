458d518a091d99e1b029fe4c31e169085d153b36
Nguyễn Ngọc Truyện <nntruyen027@gmail.com>
2025-12-25 11:09:19 +0700

    Thêm chức năng thời khóa biểu

--- src/services/hoc-sinh/thoi-khoa-bieu.js
+++ src/services/hoc-sinh/thoi-khoa-bieu.js
@@ -1,11 +1,20 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_BE + '/hoc-sinh/thoi-khoa-bieu',
+    baseURL: process.env.NEXT_PUBLIC_BE + '/hoc-sinh/thoi-khoa-bieu/',
     headers: {
         "Content-Type": "application/json",
-        "Authorization": `Bearer ${localStorage.getItem("jwtToken")}`,
     }
+});
+
+api.interceptors.request.use((config) => {
+    if (typeof window !== "undefined") {
+        const token = localStorage.getItem("jwtToken");
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+    }
+    return config;
 });
 
 export async function layDsTkb() {
